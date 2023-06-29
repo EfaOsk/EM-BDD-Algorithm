a0d4fdb83bcb8da6264011a41262a779fc3a5be4
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2023-06-29 16:08:32 +0000

    implement HMM

--- HMM.c
+++ HMM.c
@@ -1,13 +1,24 @@
 #include "HMM.h"
 #include<string.h>
 
-
-static struct HMM init(const int n_s, const int n_o, struct DoubleMatrix A, struct DoubleMatrix B, struct DoubleArray C, const char *name)
+/**
+ * Initializes a Hidden Markov Model (HMM) structure.
+ *
+ * @param N     The number of states.
+ * @param M     The number of observations.
+ * @param A     The state transition matrix.
+ * @param B     The observation emission matrix.
+ * @param C     The initial state distribution.
+ * @param name  The name of the HMM.
+ *
+ * @return The initialized HMM structure.
+ */
+static struct HMM init(const int N, const int M, struct DoubleMatrix A, struct DoubleMatrix B, struct DoubleArray C, const char *name)
 {
 
     struct HMM ret=(struct HMM){
-        .n_s=n_s,
-        .n_o=n_o,
+        .N=N,
+        .M=M,
         .A=A,
         .B=B,
         .C=C,
