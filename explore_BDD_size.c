1b4c0dfe3a749b31a9d8e8012cec81cf41e2209a
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2024-05-05 15:42:16 +0000

    Update explore_BDD_size.c

--- explore_BDD_size.c
+++ explore_BDD_size.c
@@ -2,69 +2,59 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 #include "BDD.h"
 #include "cudd.h"
 
 int main(int argc, char *argv[]) {
-   
-    int T = 2;
-    int num_sequences = 4;
-    double epsilon =  0.01;
-    char logs_folder[256]; 
-    int **obs_seq;
-    int N = 2;
-    int M = 2;
-    obs_seq = malloc(num_sequences * sizeof(int*));
-
-    
-    for (int j = 0; j < num_sequences; j++) {
-        obs_seq[j] = generate_sequence(M, T);
-    } 
-    obs_seq[0][0] = 0;
-    obs_seq[0][1] = 3;
-    obs_seq[0][2] = 2;
-    obs_seq[0][3] = 3;
-    obs_seq[0][4] = 2;
-
-    printf("%d, ", obs_seq[0][0]);
-    printf("%d, ", obs_seq[0][1]);
-    printf("%d, ", obs_seq[0][2]);
-    // printf("%d, ", obs_seq[0][3]);
-    // printf("%d\n", obs_seq[0][4]);
-    
-    DdManager *manager = Cudd_Init(0,0,CUDD_UNIQUE_SLOTS,CUDD_CACHE_SLOTS, 0);  
-    Cudd_AutodynEnable(manager, CUDD_REORDER_SAME);
-    
-    int **lookup_table_variables;
-    lookup_table_variables = (int **)malloc((N*(M-1)+(N-1)+(N*(N-1))) * sizeof(int *));
-    for (int id = 0; id <(N*(M-1)+(N-1)+(N*(N-1))); id++) {
-        (lookup_table_variables)[id] = (int *)malloc(4 * sizeof(int));
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <T>\n", argv[0]);
+        return 1;
     }
 
-    printf("Building the BDD ...\n");
+    int T = 6;  // Get T from command line arguments
+    int num_sequences = 100;
+    double epsilon = 0.01;
+    char logs_folder[256];
+    int **obs_seq;
+    int N = atoi(argv[1]);
+    int M = 4;
+    obs_seq = malloc(num_sequences * sizeof(int*));
+
+    for (int j = 0; j < num_sequences; j++) {
+        obs_seq[j] = generate_sequence(M, T);
+    }
+
+    DdManager *manager = Cudd_Init(0, 0, CUDD_UNIQUE_SLOTS, CUDD_CACHE_SLOTS, 0);
+    Cudd_AutodynEnable(manager, CUDD_REORDER_SAME);
+
+    int **lookup_table_variables;
+    lookup_table_variables = (int **)malloc((N*T*(M-1)+(N-1)+T*(N*(N-1))) * sizeof(int *));
+    for (int id = 0; id < (N*T*(M-1)+(N-1)+T*(N*(N-1))); id++) {
+        lookup_table_variables[id] = (int *)malloc(4 * sizeof(int));
+    }
+
+    // printf("Building the BDD ...\n");
     DdNode **F_obs = build_F_seq(manager, N, M, num_sequences, T, obs_seq, lookup_table_variables);
 
-
-    printf("N : %d | ", N );
-    printf("M : %d | ", M );
-    printf("T : %d | ", T );
+    printf("N : %d | ", N);
+    printf("M : %d | ", M);
+    printf("T : %d | ", T);
     printf("Encode: TRUE | ");
-    printf("DdManager vars: %d | ", Cudd_ReadSize(manager) ); // Returns the number of BDD variables in existence
-    printf("DdManager nodes: %ld | ", Cudd_ReadNodeCount(manager) ); // Reports the number of live nodes in BDDs and ADDs
-    printf("DdManager reorderings: %d | ", Cudd_ReadReorderings(manager) ); // Returns the number of times reordering has occurred
-    printf("DdManager memory: %ld \n", Cudd_ReadMemoryInUse(manager) ); // Returns the memory in use by the manager measured in bytes
+    printf("DdManager vars: %d | ", Cudd_ReadSize(manager));
+    printf("DdManager nodes: %ld | ", Cudd_ReadNodeCount(manager));
+    printf("DdManager reorderings: %d | ", Cudd_ReadReorderings(manager));
+    printf("DdManager memory: %ld \n", Cudd_ReadMemoryInUse(manager));
 
-    Cudd_PrintDebug(manager, F_obs[0], 0, 3);
-    FILE *outfile; // output file pointer for .dot file
-    outfile = fopen("test.dot","w");
-    // DdNode **ddnodearray = (DdNode**)malloc(sizeof(DdNode*)); // initialize the function array
-    // ddnodearray[0] = dd;
-    Cudd_DumpDot(manager, 4, F_obs, NULL, NULL, outfile);
+    // Cudd_PrintDebug(manager, F_obs[0], 0, 3);
+    // FILE *outfile;
+    // outfile = fopen("test.dot", "w");
+    // Cudd_DumpDot(manager, 4, F_obs, NULL, NULL, outfile);
+
     for (int i = 0; i < num_sequences; i++) {
         free(obs_seq[i]);
     }
-    // Free the allocated memory after use
     free(obs_seq);
     return 0;
 }
